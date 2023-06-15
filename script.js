be4de4ba07165c0559438f49a69b2e300f28129b
thaynafxavier <thaynafxavier@hotmail.com>
2023-06-15 15:37:09 -0300

    redes sociais

--- script.js
+++ script.js
@@ -13,8 +13,10 @@ const titulo = document.querySelector('.digitando');
 ativaLetra(titulo);
 
 const ativaMenu = document.querySelector(' .fa-bars');
+const navMenu = document.querySelector('header .navegacao-primaria')
 
 ativaMenu.addEventListener('click' , ()=>{
     ativaMenu.classList.toggle('fa-x') 
+    navMenu.classList.toggle('ativado')
 })
 
